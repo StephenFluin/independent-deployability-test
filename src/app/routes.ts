1f1c8d23b8e01b08be4d7fbef2d8d9ddcf70d253
Stephen Fluin <stephen.fluin@gmail.com>
2020-11-09 14:57:59 -0800

    add crazy idea

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,5 +4,12 @@ import { loadRemoteModule } from 'src/utils';
 
 export const routes: Routes = [{
   path: 'about', loadChildren: () =>
-    loadRemoteModule({ exposedModule: './about.module', 'remoteEntry': '//localhost:4201/remoteEntry.js', remoteName: 'remote1' }).then(m => m.AboutModule)
-}];
+    loadRemoteModule({ exposedModule: './about.module', 'remoteEntry': '//localhost:4201/remoteEntry.js', remoteName: 'remote1' }).then(m => m.AboutModule),
+}
+//   Not sure syntax here, but this is theoretically possible
+// {
+//     path: 'load/:module/:entry/:name',
+//   loadChildren: () =>
+//   loadRemoteModule({ exposedModule: './about.module', 'remoteEntry': '//localhost:4201/remoteEntry.js', remoteName: 'remote1' }).then(m => m.AboutModule),
+// }
+];
